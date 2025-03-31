7f2c0e731f89bfc62add12349a6a06b3ed484f66
BetterLordWilliam <willjordan05@icloud.com>
2025-03-31 15:08:19 -0700

    WO: DAO started, database setup working.

--- app/src/main/java/com/example/comp3717_wo_miniapp/data/EldenRingData.kt
+++ app/src/main/java/com/example/comp3717_wo_miniapp/data/EldenRingData.kt
@@ -1,5 +1,7 @@
 package com.example.comp3717_wo_miniapp.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 
@@ -10,69 +12,81 @@ interface ItemData {
     val imageUrl:       String
 }
 
+@Entity(tableName = "er_number_stats")
 data class NumericStatValue (
-    val name: String,
-    val amount: Double
+    val name:       String,
+    val amount:     Double
 )
 
+@Entity(tableName = "er_string_stats")
 data class StringStatValue (
-    val name: String,
-    val scaling: String
+    val name:       String,
+    val scaling:    String
 )
 
+@Entity(tableName = "er_items")
 data class Item (
-    override val id:             String,
-    override val name:           String,
+    @PrimaryKey
+    override val id:            String,
+    override val name:          String,
     @SerializedName("image")
-    override val imageUrl:       String,
-    override val description:    String,
-    val type:           String,
-    val effect:         String
+    override val imageUrl:      String,
+    override val description:   String,
+    val type:                   String,
+    val effect:                 String
 ) : ItemData
 
+@Entity(tableName = "er_weapons")
 data class Weapon (
-    override val id:             String,
-    override val name:           String,
+    @PrimaryKey
+    override val id:            String,
+    override val name:          String,
     @SerializedName("image")
-    override val imageUrl:       String,
-    override val description:    String,
-    val category:       String,
-    val weight:         Double,
-    val attack:         List<NumericStatValue>,
-    val defence:        List<NumericStatValue>,
+    override val imageUrl:      String,
+    override val description:   String,
+    val category:               String,
+    val weight:                 Double,
+    val attack:                 List<NumericStatValue>,
+    val defence:                List<NumericStatValue>,
     @SerializedName("requiredAttributes")
-    val reqAt:          List<NumericStatValue>,
-    val scalesWith:     List<StringStatValue>
+    val reqAt:                  List<NumericStatValue>,
+    val scalesWith:             List<StringStatValue>
 ) : ItemData
 
+@Entity(tableName = "er_armours")
 data class Armour(
-    override val id:             String,
-    override val name:           String,
+    @PrimaryKey
+    override val id:            String,
+    override val name:          String,
     @SerializedName("image")
-    override val imageUrl:       String,
-    override val description:    String,
-    val category:       String,
-    val weight:         Double,
-    val dmgNegation:    List<NumericStatValue>,
-    val resistance:     List<NumericStatValue>
+    override val imageUrl:      String,
+    override val description:   String,
+    val category:               String,
+    val weight:                 Double,
+    val dmgNegation:            List<NumericStatValue>,
+    val resistance:             List<NumericStatValue>
 ) : ItemData
 
+@Entity(tableName = "er_shields")
 data class Shield(
-    override val id:             String,
-    override val name:           String,
+    @PrimaryKey
+    override val id:            String,
+    override val name:          String,
     @SerializedName("image")
-    override val imageUrl:       String,
-    override val description:    String,
-    val category:       String,
-    val weight:         Double,
-    val attack:         List<NumericStatValue>,
-    val defence:        List<NumericStatValue>,
+    override val imageUrl:      String,
+    override val description:   String,
+    val category:               String,
+    val weight:                 Double,
+    val attack:                 List<NumericStatValue>,
+    val defence:                List<NumericStatValue>,
     @SerializedName("requiredAttributes")
-    val reqAt:          List<NumericStatValue>,
-    val scalesWith:     List<StringStatValue>
+    val reqAt:                  List<NumericStatValue>,
+    val scalesWith:             List<StringStatValue>
 ) : ItemData
 
+@Entity(tableName = "er_talismans")
 data class Talisman(
+    @PrimaryKey
     override val id:             String,
     override val name:           String,
     @SerializedName("image")
@@ -81,7 +95,9 @@ data class Talisman(
     val effects:        String
 ) : ItemData
 
+@Entity(tableName = "er_sorceries")
 data class Sorcery(
+    @PrimaryKey
     override val id:             String,
     override val name:           String,
     @SerializedName("image")
@@ -94,7 +110,9 @@ data class Sorcery(
     val requires:       List<NumericStatValue>
 ) : ItemData
 
+@Entity(tableName = "er_incantations")
 data class Incantation(
+    @PrimaryKey
     override val id:             String,
     override val name:           String,
     @SerializedName("image")
---
+++ app/src/main/java/com/example/comp3717_wo_miniapp/data/WeaponDao.kt
@@ -0,0 +1,8 @@
+package com.example.comp3717_wo_miniapp.data
+
+import androidx.room.Dao
+
+@Dao
+class WeaponDao {
+    
+}
